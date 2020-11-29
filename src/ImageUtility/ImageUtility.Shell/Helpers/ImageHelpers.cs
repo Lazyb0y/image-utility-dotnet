caf247e3ac51d0cf35fa33972a77af1f250c0013
Foysal Ahamed <thisisfoysal@gmail.com>
2020-11-29 17:00:52 +0600

    Added helper for resize image keeping aspect ratio

--- src/ImageUtility/ImageUtility.Shell/Helpers/ImageHelpers.cs
+++ src/ImageUtility/ImageUtility.Shell/Helpers/ImageHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
 
@@ -6,6 +7,25 @@ namespace ImageUtility.Shell.Helpers
 {
     public static class ImageHelpers
     {
+        public static Bitmap ResizeImageKeepingAspectRatio(Bitmap bmp, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double) maxWidth / bmp.Width;
+            var ratioY = (double) maxHeight / bmp.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int) (bmp.Width * ratio);
+            var newHeight = (int) (bmp.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+            {
+                graphics.DrawImage(bmp, 0, 0, newWidth, newHeight);
+            }
+
+            return newImage;
+        }
+
         public static void CompressImage(string sourcePath, string destPath, int quality)
         {
             using (var bmp = new Bitmap(sourcePath))
